d7e4a64386af4883c0ecb862f91277881685d037
Michael Deakin <mfdeakin@cs.unc.edu>
2017-02-05 12:14:24 -0700

    Implement nd_array outer slicing. Also fix testing and move CT_Array to it's own file

--- nd_array.hpp
+++ nd_array.hpp
@@ -4,87 +4,9 @@
 
 #include <type_traits>
 
-namespace {
-template <typename FieldT, FieldT leading, FieldT... others>
-struct CT_Array {
-  constexpr static const FieldT current = leading;
+#include "ct_array.hpp"
 
-  static constexpr int len() {
-    return sizeof...(others) + 1;
-  }
-
-  static constexpr FieldT value(int idx) {
-    return (idx == 0 ? current : Next::value(idx - 1));
-  }
-
-  static constexpr FieldT sum() {
-    return leading + Next::sum();
-  }
-
-  static constexpr FieldT product() {
-    return leading * Next::product();
-  }
-
-  template <typename... indices>
-  static constexpr int slice_idx(int idx, indices... tail) {
-    return idx * Next::product() + Next::slice_idx(tail...);
-  }
-
-  static constexpr int slice_idx(int idx) {
-    return idx * Next::product();
-  }
-
-  template <typename Idx_Array,
-            typename std::enable_if<Idx_Array::len() != 1,
-                                    int>::type = 0>
-  static constexpr FieldT slice_idx() {
-    static_assert(Idx_Array::current < leading,
-                  "Index array's indices are too large");
-    static_assert(Idx_Array::len() <= Self::len(),
-                  "Too many indices");
-    return Idx_Array::current * Next::product() +
-           Next::template slice_idx<
-               typename Idx_Array::Next>();
-  }
-
-  template <typename Idx_Array,
-            typename std::enable_if<Idx_Array::len() == 1,
-                                    int>::type = 0>
-  static constexpr FieldT slice_idx() {
-    static_assert(Idx_Array::current < leading,
-                  "Index array's indices are too large");
-    static_assert(Idx_Array::len() <= Self::len(),
-                  "Too many indices");
-    return Idx_Array::current * Next::product();
-  }
-
-  using Self = CT_Array<FieldT, leading, others...>;
-  using Next = CT_Array<FieldT, others...>;
-};
-
-template <typename FieldT, FieldT val>
-struct CT_Array<FieldT, val> {
-  constexpr static const FieldT current = val;
-
-  static constexpr int len() { return 1; }
-  static constexpr int value(int idx) { return current; }
-  static constexpr FieldT sum() { return val; }
-  static constexpr FieldT product() { return val; }
-
-  static constexpr int slice_idx(int idx) { return idx; }
-
-  template <typename Idx_Array>
-  static constexpr FieldT slice_idx() {
-    static_assert(Idx_Array::len() == 1,
-                  "Index array not of length 1");
-    static_assert(Idx_Array::current < val,
-                  "Index array value overflow");
-    return Idx_Array::current;
-  }
-};
-}
-
-template <typename A_Type, int... dims>
+template <typename A_Type, typename Dims_CT_Array>
 class ND_Array {
  public:
   constexpr ND_Array() noexcept {}
@@ -104,66 +26,23 @@ class ND_Array {
     return vals[DIMS::slice_idx(indices...)];
   }
 
+  template <typename... int_t>
+  constexpr ND_Array<
+      A_Type, typename forward_truncate_array<
+                  sizeof...(int_t), Dims_CT_Array>::type>
+      &outer_slice(int_t... indices) noexcept {
+    using truncated_dims =
+        typename forward_truncate_array<sizeof...(int_t),
+                                        DIMS>::type;
+    using ret_type = ND_Array<A_Type, truncated_dims>;
+    return *(reinterpret_cast<ret_type *>(
+        &vals[0] +
+        DIMS::slice_idx(indices...)));
+  }
+
  private:
-  using DIMS = CT_Array<int, dims...>;
+  using DIMS = Dims_CT_Array;
   A_Type vals[DIMS::product()];
 };
-
-namespace {
-
-/* Compile Time List Tests */
-
-using Arr0 = CT_Array<int, 4>;
-
-static_assert(Arr0::len() == 1,
-              "Incorrect CT Array Length");
-static_assert(Arr0::sum() == 4, "Incorrect CT Array Sum");
-static_assert(Arr0::product() == 4,
-              "Incorrect CT Array Product");
-
-using Arr1 = CT_Array<int, 1, 2, 3, 4>;
-static_assert(Arr1::len() == 4,
-              "Incorrect CT Array Length");
-static_assert(Arr1::sum() == 10, "Incorrect CT Array Sum");
-static_assert(Arr1::product() == 24,
-              "Incorrect CT Array Product");
-
-using Arr2 = CT_Array<int, 0, 1>;
-static_assert(Arr1::slice_idx<Arr2>() == 12,
-              "Incorrect Template Slice Index");
-
-using Arr3 = CT_Array<int, 0, 0, 1>;
-static_assert(Arr1::slice_idx<Arr3>() == 4,
-              "Incorrect Template Slice Index");
-
-using Arr4 = CT_Array<int, 0, 0, 0, 1>;
-static_assert(Arr1::slice_idx<Arr4>() == 1,
-              "Incorrect Template Slice Index");
-
-static_assert(Arr1::slice_idx(0, 0, 0, 0) == 0,
-              "Incorrect Runtime Slice Index");
-static_assert(Arr1::slice_idx(0, 0, 0, 1) == 1,
-              "Incorrect Runtime Slice Index");
-static_assert(Arr1::slice_idx(0, 0, 0, 2) == 2,
-              "Incorrect Runtime Slice Index");
-static_assert(Arr1::slice_idx(0, 0, 0, 3) == 3,
-              "Incorrect Runtime Slice Index");
-static_assert(Arr1::slice_idx(0, 0, 1, 0) == 4,
-              "Incorrect Runtime Slice Index");
-static_assert(Arr1::slice_idx(0, 0, 1, 1) == 5,
-              "Incorrect Runtime Slice Index");
-static_assert(Arr1::slice_idx(0, 0, 1, 2) == 6,
-              "Incorrect Runtime Slice Index");
-static_assert(Arr1::slice_idx(0, 0, 1, 3) == 7,
-              "Incorrect Runtime Slice Index");
-static_assert(Arr1::slice_idx(0, 0, 2, 0) == 8,
-              "Incorrect Runtime Slice Index");
-static_assert(Arr1::slice_idx(0, 0, 2, 1) == 9,
-              "Incorrect Runtime Slice Index");
-static_assert(Arr1::slice_idx(0, 0, 2, 2) == 10,
-              "Incorrect Runtime Slice Index");
-static_assert(Arr1::slice_idx(0, 0, 2, 3) == 11,
-              "Incorrect Runtime Slice Index");
-}
 
 #endif
--- tests.cpp
+++ tests.cpp
@@ -4,8 +4,8 @@
 
 #include "nd_array.hpp"
 
-TEST_CASE("get and set", "[ND_Array]") {
-  ND_Array<int, 2, 3, 5> arr;
+TEST_CASE("get, set, slice", "[ND_Array]") {
+  ND_Array<int, CT_Array<int, 2, 3, 5> > arr;
   int count = 1;
   for(int i = 0; i < 2; ++i) {
     for(int j = 0; j < 3; ++j) {
@@ -24,4 +24,108 @@ TEST_CASE("get and set", "[ND_Array]") {
       }
     }
   }
+  ND_Array<int, CT_Array<int, 3, 5> > &slice1 =
+      arr.outer_slice(0);
+  count = 1;
+  for(int j = 0; j < 3; ++j) {
+    for(int k = 0; k < 5; ++k) {
+      REQUIRE(slice1(j, k) == count);
+      count++;
+    }
+  }
+  ND_Array<int, CT_Array<int, 3, 5> > &slice2 =
+      arr.outer_slice(1);
+  for(int j = 0; j < 3; ++j) {
+    for(int k = 0; k < 5; ++k) {
+      REQUIRE(slice2(j, k) == count);
+      count++;
+    }
+  }
 }
+
+/* Compile Time List Tests */
+
+using Arr0 = CT_Array<int, 4>;
+
+static_assert(Arr0::len() == 1,
+              "Incorrect CT Array Length");
+static_assert(Arr0::sum() == 4, "Incorrect CT Array Sum");
+static_assert(Arr0::product() == 4,
+              "Incorrect CT Array Product");
+static_assert(Arr0::slice_idx(0) == 0,
+              "Incorrect 1D slice_idx");
+static_assert(Arr0::slice_idx(1) == 1,
+              "Incorrect 1D slice_idx");
+static_assert(Arr0::slice_idx(2) == 2,
+              "Incorrect 1D slice_idx");
+static_assert(Arr0::slice_idx(3) == 3,
+              "Incorrect 1D slice_idx");
+
+using Arr1 = CT_Array<int, 1, 2, 3, 4>;
+static_assert(Arr1::len() == 4,
+              "Incorrect CT Array Length");
+static_assert(Arr1::sum() == 10, "Incorrect CT Array Sum");
+static_assert(Arr1::product() == 24,
+              "Incorrect CT Array Product");
+
+using Arr2 = CT_Array<int, 0, 1>;
+static_assert(Arr1::slice_idx<Arr2>() == 12,
+              "Incorrect Template Slice Index");
+
+using Arr3 = CT_Array<int, 0, 0, 1>;
+static_assert(Arr1::slice_idx<Arr3>() == 4,
+              "Incorrect Template Slice Index");
+
+using Arr4 = CT_Array<int, 0, 0, 0, 1>;
+static_assert(Arr1::slice_idx<Arr4>() == 1,
+              "Incorrect Template Slice Index");
+
+static_assert(Arr1::slice_idx(0, 0, 0, 0) == 0,
+              "Incorrect Runtime Slice Index");
+static_assert(Arr1::slice_idx(0, 0, 0, 1) == 1,
+              "Incorrect Runtime Slice Index");
+static_assert(Arr1::slice_idx(0, 0, 0, 2) == 2,
+              "Incorrect Runtime Slice Index");
+static_assert(Arr1::slice_idx(0, 0, 0, 3) == 3,
+              "Incorrect Runtime Slice Index");
+static_assert(Arr1::slice_idx(0, 0, 1, 0) == 4,
+              "Incorrect Runtime Slice Index");
+static_assert(Arr1::slice_idx(0, 0, 1, 1) == 5,
+              "Incorrect Runtime Slice Index");
+static_assert(Arr1::slice_idx(0, 0, 1, 2) == 6,
+              "Incorrect Runtime Slice Index");
+static_assert(Arr1::slice_idx(0, 0, 1, 3) == 7,
+              "Incorrect Runtime Slice Index");
+static_assert(Arr1::slice_idx(0, 0, 2, 0) == 8,
+              "Incorrect Runtime Slice Index");
+static_assert(Arr1::slice_idx(0, 0, 2, 1) == 9,
+              "Incorrect Runtime Slice Index");
+static_assert(Arr1::slice_idx(0, 0, 2, 2) == 10,
+              "Incorrect Runtime Slice Index");
+static_assert(Arr1::slice_idx(0, 0, 2, 3) == 11,
+              "Incorrect Runtime Slice Index");
+
+static_assert(
+    forward_truncate_array<0, Arr1>::type::len() == 4,
+    "forward_truncate_array failed");
+static_assert(
+    forward_truncate_array<0, Arr1>::type::value(0) == 1,
+    "forward_truncate_array failed");
+static_assert(
+    forward_truncate_array<1, Arr1>::type::len() == 3,
+    "forward_truncate_array failed");
+static_assert(
+    forward_truncate_array<1, Arr1>::type::value(0) == 2,
+    "forward_truncate_array failed");
+static_assert(
+    forward_truncate_array<2, Arr1>::type::len() == 2,
+    "forward_truncate_array failed");
+static_assert(
+    forward_truncate_array<2, Arr1>::type::value(0) == 3,
+    "forward_truncate_array failed");
+static_assert(
+    forward_truncate_array<3, Arr1>::type::len() == 1,
+    "forward_truncate_array failed");
+static_assert(
+    forward_truncate_array<3, Arr1>::type::value(0) == 4,
+    "forward_truncate_array failed");
