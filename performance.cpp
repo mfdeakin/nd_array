9e418a90143965c50478f09a72f7aab2da4a3c99
michael <mfdeakin@cs.unc.edu>
2019-06-08 18:49:31 -0700

    Add iterator performance comparisons. TODO: Substantially improve the ND Array iterator performance. Maybe just have it wrap a pointer?

--- performance.cpp
+++ performance.cpp
@@ -14,6 +14,8 @@ void initialize() {}
 
 #include "nd_array.hpp"
 
+#include "zip.hpp"
+
 static void BM_Null(benchmark::State &state) {
   while(state.KeepRunning()) {
   }
@@ -53,7 +55,8 @@ static void BM_C_Array_Iterate(benchmark::State &state) {
   }
 }
 
-static void BM_ND_Array_Iterate(benchmark::State &state) {
+static void BM_ND_Array_Iterate_Index(
+    benchmark::State &state) {
   ND_Array<double, 5, 7, 11, 13, 17> array;
   benchmark::DoNotOptimize(array);
   while(state.KeepRunning()) {
@@ -68,6 +71,38 @@ static void BM_ND_Array_Iterate(benchmark::State &state) {
           }
         }
       }
+    }
+  }
+}
+
+static void BM_ND_Array_Iterate_Pointer(
+    benchmark::State &state) {
+  ND_Array<double, 5, 7, 11, 13, 17> array;
+  benchmark::DoNotOptimize(array);
+  while(state.KeepRunning()) {
+    double *iter = &array(0, 0, 0, 0, 0);
+    for(int i1 = 0; i1 < array.extent(0); i1++) {
+      for(int i2 = 0; i2 < array.extent(1); i2++) {
+        for(int i3 = 0; i3 < array.extent(2); i3++) {
+          for(int i4 = 0; i4 < array.extent(3); i4++) {
+            for(int i5 = 0; i5 < array.extent(4); i5++) {
+              benchmark::DoNotOptimize(*iter);
+              iter++;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+static void BM_ND_Array_Iterate_Iterator(
+    benchmark::State &state) {
+  ND_Array<double, 5, 7, 11, 13, 17> array;
+  benchmark::DoNotOptimize(array);
+  while(state.KeepRunning()) {
+    for(auto i = array.begin(); i != array.end(); i++) {
+      benchmark::DoNotOptimize(*i);
     }
   }
 }
@@ -98,7 +133,7 @@ static void BM_C_Array_Initialize(benchmark::State &state) {
   }
 }
 
-static void BM_ND_Array_Initialize(
+static void BM_ND_Array_Initialize_Index(
     benchmark::State &state) {
   ND_Array<double, 5, 7, 11, 13, 17> array;
   double counter = 1.0;
@@ -119,6 +154,200 @@ static void BM_ND_Array_Initialize(
   }
 }
 
+static void BM_ND_Array_Initialize_Pointer(
+    benchmark::State &state) {
+  ND_Array<double, 5, 7, 11, 13, 17> array;
+  benchmark::DoNotOptimize(array);
+  double counter = 1.0;
+  while(state.KeepRunning()) {
+    double *iter = &array(0, 0, 0, 0, 0);
+    for(int i1 = 0; i1 < array.extent(0); i1++) {
+      for(int i2 = 0; i2 < array.extent(1); i2++) {
+        for(int i3 = 0; i3 < array.extent(2); i3++) {
+          for(int i4 = 0; i4 < array.extent(3); i4++) {
+            for(int i5 = 0; i5 < array.extent(4); i5++) {
+              benchmark::DoNotOptimize(*iter = counter);
+              iter++;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+static void BM_ND_Array_Initialize_Iterator(
+    benchmark::State &state) {
+  ND_Array<double, 5, 7, 11, 13, 17> array;
+  benchmark::DoNotOptimize(array);
+  double counter = 1.0;
+  while(state.KeepRunning()) {
+    for(auto i = array.begin(); i != array.end(); i++) {
+      benchmark::DoNotOptimize(*i = counter);
+    }
+  }
+}
+
+static void BM_ND_Array_Iterate_2_Index(
+    benchmark::State &state) {
+  using array_t = ND_Array<double, 5, 7, 11, 13, 17>;
+  using size_type = array_t::size_type;
+  array_t a1, a2;
+  while(state.KeepRunning()) {
+    for(size_type i1 = 0; i1 < a1.extent(0); i1++) {
+      for(size_type i2 = 0; i2 < a1.extent(0); i2++) {
+        for(size_type i3 = 0; i3 < a1.extent(0); i3++) {
+          for(size_type i4 = 0; i4 < a1.extent(0); i4++) {
+            for(size_type i5 = 0; i5 < a1.extent(0); i5++) {
+              benchmark::DoNotOptimize(
+                  a1(i1, i2, i3, i4, i5));
+              benchmark::DoNotOptimize(
+                  a2(i1, i2, i3, i4, i5));
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+static void BM_ND_Array_Iterate_2_Pointer(
+    benchmark::State &state) {
+  using array_t = ND_Array<double, 5, 7, 11, 13, 17>;
+  using size_type = array_t::size_type;
+  array_t a1, a2;
+  while(state.KeepRunning()) {
+    double *v1 = &a1(0, 0, 0, 0, 0);
+    double *v2 = &a2(0, 0, 0, 0, 0);
+    for(size_type i1 = 0; i1 < a1.extent(0); i1++) {
+      for(size_type i2 = 0; i2 < a1.extent(0); i2++) {
+        for(size_type i3 = 0; i3 < a1.extent(0); i3++) {
+          for(size_type i4 = 0; i4 < a1.extent(0); i4++) {
+            for(size_type i5 = 0; i5 < a1.extent(0); i5++) {
+              benchmark::DoNotOptimize(*v1);
+              v1++;
+              benchmark::DoNotOptimize(*v2);
+              v2++;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+static void BM_ND_Array_Iterate_2_Iterator(
+    benchmark::State &state) {
+  using array_t = ND_Array<double, 5, 7, 11, 13, 17>;
+  array_t a1, a2;
+  while(state.KeepRunning()) {
+    for(auto i1 = a1.begin(), i2 = a2.begin();
+        i1 != a1.end(); i1++, i2++) {
+      benchmark::DoNotOptimize(*i1);
+      benchmark::DoNotOptimize(*i2);
+    }
+  }
+}
+
+static void BM_ND_Array_Iterate_2_Zip(
+    benchmark::State &state) {
+  using array_t = ND_Array<double, 5, 7, 11, 13, 17>;
+  array_t a1, a2;
+  using zip_t = Zip<array_t, array_t>;
+  while(state.KeepRunning()) {
+    for(auto [v1, v2] : zip_t(a1, a2)) {
+      benchmark::DoNotOptimize(v1);
+      benchmark::DoNotOptimize(v2);
+    }
+  }
+}
+
+static void BM_ND_Array_Initialize_2_Index(
+    benchmark::State &state) {
+  using array_t = ND_Array<double, 5, 7, 11, 13, 17>;
+  using size_type = array_t::size_type;
+  array_t a1, a2;
+  double counter = 1.0;
+  while(state.KeepRunning()) {
+    for(size_type i1 = 0; i1 < a1.extent(0); i1++) {
+      for(size_type i2 = 0; i2 < a1.extent(0); i2++) {
+        for(size_type i3 = 0; i3 < a1.extent(0); i3++) {
+          for(size_type i4 = 0; i4 < a1.extent(0); i4++) {
+            for(size_type i5 = 0; i5 < a1.extent(0); i5++) {
+              benchmark::DoNotOptimize(
+                  a1(i1, i2, i3, i4, i5) = counter);
+              counter += 1.0;
+              benchmark::DoNotOptimize(
+                  a2(i1, i2, i3, i4, i5) = counter);
+              counter += 1.0;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+static void BM_ND_Array_Initialize_2_Pointer(
+    benchmark::State &state) {
+  using array_t = ND_Array<double, 5, 7, 11, 13, 17>;
+  using size_type = array_t::size_type;
+  array_t a1, a2;
+  double counter = 1.0;
+  while(state.KeepRunning()) {
+    double *v1 = &a1(0, 0, 0, 0, 0);
+    double *v2 = &a2(0, 0, 0, 0, 0);
+    for(size_type i1 = 0; i1 < a1.extent(0); i1++) {
+      for(size_type i2 = 0; i2 < a1.extent(0); i2++) {
+        for(size_type i3 = 0; i3 < a1.extent(0); i3++) {
+          for(size_type i4 = 0; i4 < a1.extent(0); i4++) {
+            for(size_type i5 = 0; i5 < a1.extent(0); i5++) {
+              benchmark::DoNotOptimize(*v1 = counter);
+              v1++;
+              counter += 1.0;
+              benchmark::DoNotOptimize(*v2 = counter);
+              v2++;
+              counter += 1.0;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+static void BM_ND_Array_Initialize_2_Iterator(
+    benchmark::State &state) {
+  using array_t = ND_Array<double, 5, 7, 11, 13, 17>;
+  array_t a1, a2;
+  double counter = 1.0;
+  while(state.KeepRunning()) {
+    for(auto i1 = a1.begin(), i2 = a2.begin();
+        i1 != a1.end(); i1++, i2++) {
+      benchmark::DoNotOptimize(*i1 = counter);
+      counter += 1.0;
+      benchmark::DoNotOptimize(*i2 = counter);
+      counter += 1.0;
+    }
+  }
+}
+
+static void BM_ND_Array_Initialize_2_Zip(
+    benchmark::State &state) {
+  using array_t = ND_Array<double, 5, 7, 11, 13, 17>;
+  array_t a1, a2;
+  double counter = 1.0;
+  using zip_t = Zip<array_t, array_t>;
+  while(state.KeepRunning()) {
+    for(auto [v1, v2] : zip_t(a1, a2)) {
+      benchmark::DoNotOptimize(v1 = counter);
+      counter += 1.0;
+      benchmark::DoNotOptimize(v2 = counter);
+      counter += 1.0;
+    }
+  }
+}
+
 int main(int argc, char **argv) {
   Kokkos::initialize();
 
@@ -128,13 +357,52 @@ int main(int argc, char **argv) {
 
   benchmark::RegisterBenchmark("BM_C_Array_Iterate",
                                BM_C_Array_Iterate);
-  benchmark::RegisterBenchmark("BM_ND_Array_Iterate",
-                               BM_ND_Array_Iterate);
+  benchmark::RegisterBenchmark("BM_ND_Array_Iterate_Index",
+                               BM_ND_Array_Iterate_Index);
+  benchmark::RegisterBenchmark(
+      "BM_ND_Array_Iterate_Pointer",
+      BM_ND_Array_Iterate_Pointer);
+  benchmark::RegisterBenchmark(
+      "BM_ND_Array_Iterate_Iterator",
+      BM_ND_Array_Iterate_Iterator);
 
   benchmark::RegisterBenchmark("BM_C_Array_Initialize",
                                BM_C_Array_Initialize);
-  benchmark::RegisterBenchmark("BM_ND_Array_Initialize",
-                               BM_ND_Array_Initialize);
+  benchmark::RegisterBenchmark(
+      "BM_ND_Array_Initialize_Index",
+      BM_ND_Array_Initialize_Index);
+  benchmark::RegisterBenchmark(
+      "BM_ND_Array_Initialize_Pointer",
+      BM_ND_Array_Initialize_Pointer);
+  benchmark::RegisterBenchmark(
+      "BM_ND_Array_Initialize_Iterator",
+      BM_ND_Array_Initialize_Iterator);
+
+  benchmark::RegisterBenchmark(
+      "BM_ND_Array_Iterate_2_Index",
+      BM_ND_Array_Iterate_2_Index);
+  benchmark::RegisterBenchmark(
+      "BM_ND_Array_Iterate_2_Pointer",
+      BM_ND_Array_Iterate_2_Pointer);
+  benchmark::RegisterBenchmark(
+      "BM_ND_Array_Iterate_2_Iterator",
+      BM_ND_Array_Iterate_2_Iterator);
+  benchmark::RegisterBenchmark("BM_ND_Array_Iterate_2_Zip",
+                               BM_ND_Array_Iterate_2_Zip);
+
+  benchmark::RegisterBenchmark(
+      "BM_ND_Array_Initialize_2_Index",
+      BM_ND_Array_Initialize_2_Index);
+  benchmark::RegisterBenchmark(
+      "BM_ND_Array_Initialize_2_Pointer",
+      BM_ND_Array_Initialize_2_Pointer);
+  benchmark::RegisterBenchmark(
+      "BM_ND_Array_Initialize_2_Iterator",
+      BM_ND_Array_Initialize_2_Iterator);
+  benchmark::RegisterBenchmark(
+      "BM_ND_Array_Initialize_2_Zip",
+      BM_ND_Array_Initialize_2_Zip);
+
   benchmark::Initialize(&argc, argv);
   benchmark::RunSpecifiedBenchmarks();
   return 0;
